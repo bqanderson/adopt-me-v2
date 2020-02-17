8fbd70123c8c18bd9c0455cd13923de1ac9ceb9c
Brian Q Anderson <bqanderson@me.com>
2020-02-17 13:35:55 -0600

    add emotion for css

---
+++ src/NavBar.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Link } from '@reach/router'
+import { css, keyframes } from '@emotion/core'
+import colors from './colors'
+
+const spin = keyframes`
+  to {
+    transform: rotate(360deg)
+  }
+`
+
+const NavBar = () => (
+  <header
+    css={css`
+      background-color: ${colors.secondary};
+      padding: 15px;
+      margin: 25px auto;
+      border-radius: 9px;
+    `}
+  >
+    <Link to='/'>Adopt Me!</Link>
+    <span
+      css={css`
+        font-size: 60px;
+        display: inline-block;
+        animation: 3s ${spin} linear infinite;
+
+        &:hover {
+          cursor: default;
+          animation: 3s ${spin} linear infinite reverse;
+        }
+      `}
+      role='img'
+      aria-label='logo'
+    >
+      🐕
+    </span>
+  </header>
+)
+
+export default NavBar
