cfefcc28dc7f73efc4f91e367c5204f92ba6e908
Brian Q Anderson <bqanderson@me.com>
2020-04-04 22:11:22 -0500

    Refactor away from Redux for testing.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
-import { Router } from '@reach/router'
-import { Provider } from 'react-redux'
+import { Router, Link } from '@reach/router'
 import Details from './Details'
 import SearchParams from './SearchParams'
-import NavBar from './NavBar'
-import store from './store'
+import ThemeContext from './ThemeContext'
 
 const App = () => {
+  const theme = useState('darkblue')
   return (
-    <Provider store={store}>
+    <ThemeContext.Provider value={theme}>
       <div>
-        <NavBar />
+        <header>
+          <Link to='/'>Adopt Me!</Link>
+        </header>
         <Router>
           <SearchParams path='/' />
           <Details path='/details/:id' />
         </Router>
       </div>
-    </Provider>
+    </ThemeContext.Provider>
   )
 }
 
