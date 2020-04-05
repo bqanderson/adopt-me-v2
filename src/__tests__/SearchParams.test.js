4ec1e4e25c3f19c2f441b5b6f77a64ef95a2a370
Brian Q Anderson <bqanderson@me.com>
2020-04-05 12:04:41 -0500

    Add more component tests

--- src/__tests__/SearchParams.test.js
+++ src/__tests__/SearchParams.test.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { render, cleanup } from '@testing-library/react'
+import { render, cleanup, fireEvent } from '@testing-library/react'
 import pet, { ANIMALS, _breeds, _dogs } from '@frontendmasters/pet'
 import SeachParams from '../SearchParams'
 
 afterEach(cleanup)
 
 test('SearchParams', async () => {
-  const { getByTestId } = render(<SeachParams />)
+  const { container, getByTestId, getByText } = render(<SeachParams />)
 
   const animalDropdown = getByTestId('use-dropdown-animal')
   expect(animalDropdown.children.length).toEqual(ANIMALS.length + 1)
@@ -14,4 +14,10 @@ test('SearchParams', async () => {
   expect(pet.breeds).toHaveBeenCalled()
   const breedDropdown = getByTestId('use-dropdown-breed')
   expect(breedDropdown.children.length).toEqual(_breeds.length + 1)
+
+  const searchResults = getByTestId('search-results')
+  expect(searchResults.textContent).toEqual('No Pets Found')
+  fireEvent(getByText('Submit'), new MouseEvent('click'))
+  expect(pet.animals).toHaveBeenCalled()
+  expect(searchResults.children.length).toEqual(_dogs.length)
 })
